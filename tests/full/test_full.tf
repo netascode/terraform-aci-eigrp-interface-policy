e14d825520c188794499f9f256e40d37e6e43f57
conmurphy <conormurphy33@hotmail.com>
2023-11-10 12:33:22 +0100

    Add eigrp interface policy (#1)

--- tests/full/test_full.tf
+++ tests/full/test_full.tf
@@ -13,38 +13,76 @@ terraform {
   }
 }
 
+resource "aci_rest_managed" "fvTenant" {
+  dn         = "uni/tn-TF"
+  class_name = "fvTenant"
+}
+
 module "main" {
   source = "../.."
 
-  name        = "ABC"
-  alias       = "ALIAS"
-  description = "DESCR"
+  tenant            = "TF"
+  name              = "EIGRP1"
+  description       = "My Description"
+  hello_interval    = 10
+  hold_interval     = 30
+  bandwidth         = 10
+  delay             = 20
+  delay_unit        = "pico"
+  bfd               = true
+  self_nexthop      = true
+  passive_interface = true
+  split_horizon     = true
 }
 
-data "aci_rest_managed" "fvTenant" {
-  dn = "uni/tn-ABC"
+data "aci_rest_managed" "eigrpIfPol" {
+  dn = module.main.dn
 
   depends_on = [module.main]
 }
 
-resource "test_assertions" "fvTenant" {
-  component = "fvTenant"
+resource "test_assertions" "eigrpIfPol" {
+  component = "eigrpIfPol"
 
   equal "name" {
     description = "name"
-    got         = data.aci_rest_managed.fvTenant.content.name
-    want        = "ABC"
+    got         = data.aci_rest_managed.eigrpIfPol.content.name
+    want        = module.main.name
   }
 
-  equal "nameAlias" {
-    description = "nameAlias"
-    got         = data.aci_rest_managed.fvTenant.content.nameAlias
-    want        = "ALIAS"
+  equal "helloIntvl" {
+    description = "helloIntvl"
+    got         = data.aci_rest_managed.eigrpIfPol.content.helloIntvl
+    want        = "10"
   }
 
-  equal "descr" {
-    description = "descr"
-    got         = data.aci_rest_managed.fvTenant.content.descr
-    want        = "DESCR"
+  equal "holdIntvl" {
+    description = "holdIntvl"
+    got         = data.aci_rest_managed.eigrpIfPol.content.holdIntvl
+    want        = "30"
+  }
+
+  equal "bw" {
+    description = "bw"
+    got         = data.aci_rest_managed.eigrpIfPol.content.bw
+    want        = "10"
+  }
+
+  equal "delay" {
+    description = "delay"
+    got         = data.aci_rest_managed.eigrpIfPol.content.delay
+    want        = "20"
+  }
+
+  equal "delayUnit" {
+    description = "delayUnit"
+    got         = data.aci_rest_managed.eigrpIfPol.content.delayUnit
+    want        = "pico"
+  }
+
+  equal "ctrl" {
+    description = "ctrl"
+    got         = data.aci_rest_managed.eigrpIfPol.content.ctrl
+    want        = "bfd,nh-self,passive,split-horizon"
   }
 }
